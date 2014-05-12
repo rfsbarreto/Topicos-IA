0d3adbd1295174a54493f1eb69de24eb41daa00e
Raphael Barreto <rfsbarreto@gmail.com>
2014-05-11 23:12:14 -0300

    Ajustes no Adaline Batelada

--- adaline_batelada.py
+++ adaline_batelada.py
@@ -2,20 +2,23 @@ x=[0,0.1,0.2,0.3,0.4,0.5,0.6]
 y=[0,0.035,0.07,0.105,0.14,0.175,0.21]
 eta=0.7
 A=0
-w0=0
+peso=0
 N=7
 b=0.35
 for epoca in xrange(0,75):
-	dw=eta/N
-	sum1=0
+#dw=eta/N
+	somatorio_erro_quadrado=0
+	somatorio_erro_x=0
 	for i in xrange(0,N):
-		aux=(y[i]-(x[i]*w0))*x[i]
-		print aux
-		sum1+=aux
-	print "soma: ",sum1
-	dw=dw*sum1
+		erro=(y[i]-(x[i]*peso))
+		somatorio_erro_quadrado+=pow(erro,2)
+		delta_w=erro*x[i]
+		somatorio_erro_x+=delta_w
+		print "erro: ",erro," som: ",somatorio_erro_quadrado
+	dw=(eta/N)*somatorio_erro_x
+	eqm=(1/(2.0*N))*somatorio_erro_quadrado
 #	for i in xrange(0,N):
-	w0=w0+dw
-	print w0,dw
-print w0
+	peso=peso+dw
+	print epoca,peso,eqm,(1/2.0)*somatorio_erro_quadrado
+#print w0
 
--- adaline_online.py
+++ adaline_online.py
@@ -1,6 +1,6 @@
 x=[0,0.1,0.2,0.3,0.4,0.5,0.6]
 y=[0,0.035,0.07,0.105,0.14,0.175,0.21]
-eta=0.7
+eta=0.1
 A=0
 w0=0
 N=7
@@ -8,12 +8,12 @@ b=0.35
 for epoca in xrange(0,75):
 	dw=eta
 	sum1=0
+	for i in xrange(0,N):
+		aux=(y[i]-(x[i]*w0))*x[i]
+#		print aux
+		dw=eta*aux
 #	for i in xrange(0,N):
-	aux=(y[i]-(x[i]*w0))*x[i]
-	print aux
-	dw=dw*aux
-#	for i in xrange(0,N):
-	w0=w0+dw
-	print w0,dw
+		w0=w0+dw
+		print w0,dw
 print w0
 
