cd38e325822048730a34fa6a598b36de801d99f1
Raphael Barreto <rfsbarreto@gmail.com>
2014-05-12 23:19:23 -0300

    Arquivos complementares

---
+++ Questao1-adaline_batelada.py
@@ -0,0 +1,25 @@
+x=[0,0.1,0.2,0.3,0.4,0.5,0.6]
+y=[0,0.035,0.07,0.105,0.14,0.175,0.21]
+eta=0.7
+A=0
+peso=0
+N=7
+b=0.35
+for epoca in xrange(0,75):
+#dw=eta/N
+	somatorio_erro_quadrado=0
+	somatorio_erro_x=0
+	for i in xrange(0,N):
+		erro=(y[i]-(x[i]*peso))
+		somatorio_erro_quadrado+=pow(erro,2)
+		erro_x=erro*x[i]
+		somatorio_erro_x+=erro_x
+#		print "erro: ",erro," som: ",somatorio_erro_quadrado
+	dw=(eta/N)*somatorio_erro_x
+	eqm=(1/(2.0*N))*somatorio_erro_quadrado
+#	for i in xrange(0,N):
+	peso=peso+dw
+	if ((epoca+1)%3==0):
+		print epoca,"peso:",peso,"EQM:",eqm
+#print w0
+
--- questao2.py
+++ questao2.py
@@ -15,9 +15,9 @@ for i in xrange(0,20):
 peso0,peso1,peso2,peso3=0,0,0,0
 N=8
 eta=0.1
-print "result:",resultados,"\n"
-print "x1:",x1,"\nx2:",x2,"\nx3:",x3,"x0\n",x0
-print "y1",y1,"\ny2",y2,"\ny3",y3,"y0\n",y0
+#print "result:",resultados,"\n"
+#print "x1:",x1,"\nx2:",x2,"\nx3:",x3,"x0\n",x0
+#print "y1",y1,"\ny2",y2,"\ny3",y3,"y0\n",y0
 #valores corretos
 a1,a2,a3,a0=0.565,2.657,0.674,-pi
 
@@ -27,11 +27,11 @@ for epoca in xrange(0,750):
         somatorio_erro_x=0
 	somatorio_erro_x0=0	
 	#funcao 1
-     	 
+	print "Epoca",epoca,"\n"     	 
 	for i in xrange(0,20):
-		print "pesos:",peso0,peso1,peso2,peso3
+#:		print "pesos:",peso0,peso1,peso2,peso3
                 erro=(resultados[i]-(y1[i]*peso1+y2[i]*peso2+y3[i]*peso3-peso0))
-#		somatorio_erro_quadrado+=pow(erro,2)
+		somatorio_erro_quadrado+=pow(erro,2)
                 erro_x1=erro*y1[i]
 		erro_x0=erro*y0[i]
 		erro_x2=erro*y2[i]
@@ -48,8 +48,9 @@ for epoca in xrange(0,750):
         	peso2+=dw2
        		peso3+=dw3
         	peso0+=dw0
-
 	eqm=(1/(2.0*N))*somatorio_erro_quadrado
+	print "  pesos:  peso0->",peso0,"peso1->",peso1,"peso2->",peso2,"peso3->",peso3 
+	print "  EQM:",eqm,"\n"
 #       for i in xrange(0,N):
 #        if ((epoca+1)%3==0):
 	#        print epoca,"peso0:",peso0,"peso1:",peso1,"peso2",peso2,"peso3",peso3,"EQM:",eqm
