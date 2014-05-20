4200a160755b383250dcbfb7ada4a995311f8ab5
Raphael Barreto <rfsbarreto@gmail.com>
2014-05-20 19:34:32 -0300

    comeco questao 4

--- Tarefa_4/Questao_1.r
+++ Tarefa_4/Questao_1.r
@@ -1,3 +1,48 @@
+x=[0,0.1,0.2,0.3,0.4,0.5,0.6]
+y=[0,0,0,0,0,0,0]
+eta=1
+teta=0.2
+A=0
+peso=0
+N=7
+b=0.35
+for epoca in xrange(0,75):
+#dw=eta/N
+	somatorio_erro_quadrado=0
+	somatorio_erro_x=0
+	for i in xrange(0,N):
+		erro=(y[i]-(x[i]*peso))
+		somatorio_erro_quadrado+=pow(erro,2)
+		erro_x=erro*x[i]
+		somatorio_erro_x+=erro_x
+#		print "erro: ",erro," som: ",somatorio_erro_quadrado
+	dw=(eta/N)*somatorio_erro_x
+	eqm=(1/(2.0*N))*somatorio_erro_quadrado
+#	for i in xrange(0,N):
+	peso=peso+dw
+	if ((epoca+1)%3==0):
+		print epoca,"peso:",peso,"EQM:",eqm
+#print w0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 n=7
 x = c(0.0,0.1,0.2,0.3,0.4,0.5,0.6)
 y = c(0,0.035,0.070,0.105,0.140,0.175,0.210)
@@ -67,3 +112,4 @@ while(times < 2)
   }
   epoca = epoca + 1
 }
+'''
