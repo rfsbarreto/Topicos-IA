89a59c95c4d50c61d761889cce93b6f98943981c
Raphael Barreto <rfsbarreto@gmail.com>
2014-05-20 22:52:05 -0300

    Acho que esta funcional

--- Tarefa_4/Questao_1.r
+++ Tarefa_4/Questao_1.r
@@ -1,134 +1,46 @@
 table=read.csv("inputs_questao1.csv",sep=";")
 table[1,4]
 table
-w=[0,0,0,0]
+w=c(0,0,0)
 
 eta=1
 teta=0.2
 A=0
 peso=0
-N=7
-tem_erro=1
+lines=4
+att=3
+erro_count=1
 
 tshld <- function(x){
-        ifelse(x<teta,-1,ifelse(x==teta)0,1)
+        ifelse(x<teta,-1,ifelse(x==teta,0,1))
 }
 
-
-
-while(tem_erro){
-#dw=eta/N
+epoca=0
+while(epoca<10){
+	erro_count=0
+	for(j in seq(1,lines)){
+	#	print(j)
 	
-	somatorio_erro_quadrado=0
-	somatorio_xiwi=0
-	for( i=1 in xrange(0,N):
-		#erro=(y[i]-(x[i]*peso))
-		#somatorio_erro_quadrado+=pow(erro,2)
-		x=erro*x[i]
-		somatorio_xiwi+=erro_x
-#		print "erro: ",erro," som: ",somatorio_erro_quadrado
-	y=tshld(somatorio_xiwi)
-	if y!= table(i,4)
-
-#	eqm=(1/(2.0*N))*somatorio_erro_quadrado
-
-#	for i in xrange(0,N):
-	peso=peso+dw
+		somatorio_erro_quadrado=0
+		somatorio_xiwi=0
+		for(i in seq(1,att)){
+			x=w[i]*table[j,i]
+			somatorio_xiwi=somatorio_xiwi+x;
+		}
+		cat(somatorio_xiwi,"-> sm\n");
+		y=tshld(somatorio_xiwi)
+#		print(y)
+		if (y!= table[j,4]){
+			cat(y,table[j,4],"entrou\npesos: ");
+			erro_count=erro_count+1;
+			w[1]=w[1]+ table[j,1]*eta*table[j,4]
+			w[2]=w[2]+ table[j,2]*eta*table[j,4]
+			w[3]=w[3]+ table[j,3]*eta*table[j,4]
+		}
+#		j=(j+1)%%4;
+	}
+	print(w)	
+	epoca=epoca+1;
 }
 
 
-
-#	if ((epoca+1)%3==0):
-#		print epoca,"peso:",peso,"EQM:",eqm
-#print w0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-n=7
-x = c(0.0,0.1,0.2,0.3,0.4,0.5,0.6)
-y = c(0,0.035,0.070,0.105,0.140,0.175,0.210)
-
-EQMs = c(rep(0,175))
-ws = c(rep(0,175))
-
-w = c(rep(0,n))
-saida = c(rep(0,n))
-erro = c(rep(0,n))
-dw = c(rep(0,n))
-
-a = 0.2
-precisao = 0
-
-u <- function(x,w){
-  sum(x*w)
-} 
-
-Thresh <- function(x,t){
-	ifelse(x<t,-1,ifelse(x==t)0,1)
-}
-
-EQM <- function(y,w,n){
-  (sum((y-w)^2))*(n/2)
-}
-
-w[1] = 0
-saida[1] = w[1]*x[1]
-erro[1] = y[1]-saida[1]
-dw[1] = a*x[1]*erro[1]
-
-EqmAnterior = EQM(y,w[1],n)
-EqmAtual = 0
-print(EqmAtual)
-times = 0
-epoca = 0
-eqmc = 0
-wc = 0
-while(times < 2)
-{
-  EqmAnterior = EQM(y,w[1],n)
-  ws[wc] = w[1]
-  wc = wc + 1
-  w[1] = w[n]+dw[n]
-  saida[1] = w[1]*x[1]
-  erro[1] = y[1]-saida[1]
-  dw[1] = a*x[1]*erro[1]
-  
-  for(i in 2:n){
-    ws[wc] = w[1]
-    wc = wc + 1
-    w[i] = w[i-1]+dw[i-1]
-    saida[i] = w[i]*x[i]
-    erro[i] = y[i]-saida[i]
-    dw[i] = a*x[i]*erro[i]
-  }
-  
-  ws[wc] = w[1]
-  wc = wc + 1
-
-  EqmAtual = EQM(y,w[1],n)
-  EQMs[eqmc] = abs(EqmAtual - EqmAnterior)
-  eqmc = eqmc+1
-  if(abs(EqmAtual - EqmAnterior) <= precisao){
-    times = times + 1
-  }
-  epoca = epoca + 1
-}
-'''
