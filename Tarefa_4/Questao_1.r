fb6c0003a682dd16b2454d0c04a00f25e6c4f325
Raphael Barreto <rfsbarreto@gmail.com>
2014-05-20 23:45:07 -0300

    PODE ME CHAMAR DE PAPAIRscript Questao_1.r  :P

--- Tarefa_4/Questao_1.r
+++ Tarefa_4/Questao_1.r
@@ -12,35 +12,48 @@ att=3
 erro_count=1
 
 tshld <- function(x){
-        ifelse(x<teta,-1,ifelse(x==teta,0,1))
+        ifelse(x<(-1*teta),-1,ifelse(x>teta,1,0))
 }
 
 epoca=0
 while(epoca<10){
 	erro_count=0
+	cat("Época ",epoca,":\n\n");
 	for(j in seq(1,lines)){
-	#	print(j)
-	
-		somatorio_erro_quadrado=0
 		somatorio_xiwi=0
 		for(i in seq(1,att)){
 			x=w[i]*table[j,i]
 			somatorio_xiwi=somatorio_xiwi+x;
 		}
-		cat(somatorio_xiwi,"-> sm\n");
 		y=tshld(somatorio_xiwi)
-#		print(y)
+	
 		if (y!= table[j,4]){
-			cat(y,table[j,4],"entrou\npesos: ");
 			erro_count=erro_count+1;
 			w[1]=w[1]+ table[j,1]*eta*table[j,4]
 			w[2]=w[2]+ table[j,2]*eta*table[j,4]
 			w[3]=w[3]+ table[j,3]*eta*table[j,4]
 		}
-#		j=(j+1)%%4;
+		cat("\t\tpesos passo",j,"-> ", w,"\n");
 	}
+	cat("pesos por epoca ")  
+
 	print(w)	
 	epoca=epoca+1;
 }
 
+testar = function(t1,t2) {
+    t = c(t1,t2,1)
+    saida = 0
+    for(i in 1:3){
+        saida = saida+ w[i]*t[i]
+    }
+    
+    return(tshld(saida))
+}
+
+cat("testando:\n")
+testar(1,1)
+testar(1,0)
+testar(0,1)
+testar(0,0)
 
