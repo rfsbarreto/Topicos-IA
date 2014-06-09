13b098ddedfe03698667fabd9ed590d3e14fc0c2
Raphael Barreto <rfsbarreto@gmail.com>
2014-06-09 00:19:53 -0300

    esse point ta massa

--- MLP1.r
+++ MLP1.r
@@ -17,7 +17,7 @@ unscale<- function(xl){
 	 ( (xl+1) * (max(Yd_original[1,])-min(Yd_original[1,])))/(2)+min(Yd_original[1,])
 # xl ** (max(Yd_original[1,])-min(Yd_original[1,])))/(2)+min(Yd_original[1,]))
 	}
-eta = 0.1 	#coeficiente aprendizagem
+eta = 0.4 	#coeficiente aprendizagem
 
 M =2		#Numero de camadas 
 
@@ -30,11 +30,12 @@ V = vector('list', M)		#vetor auxiliar, armazena calculo do somatorio dos pesos*
 U = vector('list', M)           #vetor auxiliar,armazena resultado da funçao ativacao
 delta = vector('list', M) 	#representa o delta minusculo
 W = vector('list', M)		#Vetor de Pesos
-
+som1=vector('list', M)
 # Geraçao de pesos aleatorios
 set.seed(10000)
 for (m in c(1:M)){
-  W[[m]] = matrix( runif( Jm[m] * En[m] ,-0.2,0.2),Jm[m])
+  W[[m]] = matrix( runif( Jm[m] * En[m] ,-0.1,0.1),Jm[m])
+  som1[[m]]=matrix(runif( Jm[m] * En[m] ,0,0),Jm[m])  
 }
 
 o=W[[1]]
@@ -59,7 +60,7 @@ Yd[1,]=(Yd[1,]-min(Yd[1,]))*2/ (max(Yd[1,])-min(Yd[1,]))-1
 #print(Yd)
 L =  length(Yd)
 L=41
-ep=400
+ep=700
 #print(sum(head(t(Yd),60))/60)
 y_ = sum( c(head(t(Yd),L)) )/L
 print("Y_")
@@ -74,18 +75,21 @@ print(y_)
  E3=matrix(0,ep,1)
  EE1=matrix(0,ep,1)
  Erro_quad= matrix(0,ep,L)
+ Ys=matrix(0,ep,L)
  Erro_abs= matrix(0,ep,L)
- erro_min= matrix(0,ep,1)
+# erro_min= matrix(0,ep,1)
  erro_max= matrix(0,ep,1)
  EErro_quad=matrix(0,ep,1)
+#som1=0
 ###################}
 
 for (epoca in 1:ep){
-#	id = t(sample(L,replace=F))
-	id = seq(1,L)
+	id = t(sample(L,replace=F))
+#	id = seq(1,L)
 #	print(id)
 	for (l in 1:L){  #1:L){
 	  X = matrix( Xd[,l] )
+	  #print(W[[1]]==W[1])
 	  V[[1]] = W[[1]] %*% X
 	  #print(t(V[[1]]))
 	  U[[1]] = tanh( V[[1]] )
@@ -95,28 +99,54 @@ for (epoca in 1:ep){
 	    U[[m]] = tanh( V[[m]] )
 	  }
 	  Y = U[[m]]
+	  Ys[epoca,l]=Y
+	  print("sai")
+	  print(Y)
+	  print(Yd[,l])
 	  Erro_quad[epoca,l]=t(Y - Yd[,l]) %*% (Y - Yd[,l])
-	 # Erro_quad[epoca,l]=Erro_quad[epoca,l]/2
+	  Erro_quad[epoca,l]=Erro_quad[epoca,l]
 	  Erro_abs[epoca,l]=t(  abs(Yd[,l]-Y)    )             
 	  EE[epoca,l]=t(Yd[,l]-y_) %*% (Yd[,l]-y_)
-	  delta[[M]] = (Yd[,l]-Y) * (1/cosh(V[[M]]) )^2 #sech(z) = 1/cosh(z)
+	  delta[[M]] = (Yd[,l]-Y) * (1/(cosh(V[[M]]))^2) #sech(z) = 1/cosh(z)
 	  #print("deltaM:")
-
+#          som1= som1 + delta[[M]] 
 	  #print(delta[[M]]) 
 	  aux= 2*eta*delta[[M]] %*% t(U[[M-1]]) #
 	 # print(aux)
 	 # print(W[[M]])	
-	  W[[M]] =W[[M]] + aux
+	 # W[[M]] =W[[M]] + aux
 	  for (m in M-1:1){
 	    teste=0.0
 	    #for ( p 
 	    #teste=delta[M]*W
-	    delta[[m]] = t(W[[m+1]]) %*% delta[[m+1]] * (1/cosh(V[[m]]) )^2 #sech(z) = 1/cosh(z)
-	    if( m == 1)
-		W[[m]] = W[[m]] + 2*eta*delta[[m]] %*% t(X)
-	    else
-		W[[m]] = W[[m]] + 2*eta*delta[[m]] %*% t(U[[m-1]]) #
+            #print("a")
+	    #print(W[[m+1]][,1])
+	    #print(delta[[m+1]])
+	    #print(t(W[[m+1]]) %*% delta[[m+1]])	
+	    #print(V[[m]][1,])
+	    delta[[m]] = (t(W[[m+1]]) %*% delta[[m+1]]) * (1/cosh(V[[m]]) )^2 #sech(z) = 1/cosh(z)
+	    
+	    #print(delta[[m]])
+	    #if( m == 1)
+#		W[[m]] = W[[m]] + 2*eta*delta[[m]] %*% t(X)
+#	    else
+#		W[[m]] = W[[m]] + 2*eta*delta[[m]] %*% t(U[[m-1]]) #
 	  }
+	  #print("MMMM")
+	  #print(M)
+          for (m in M:1)
+           if( m == 1)
+              W[[m]] = W[[m]] + eta*delta[[m]] %*% t(X)
+           else
+              W[[m]] = W[[m]] + eta*delta[[m]] %*% t(U[[m-1]]) #
+	  #for (m in M:1)
+	  # if( m == 1)
+           #    som1[[m]] = som1[[m]] + delta[[m]] %*% t(X)
+          # else
+          #     som1[[m]] = som1[[m]] + delta[[m]] %*% t(U[[m-1]]) #
+
+	 # W[[M]] =W[[M]] + aux
+
 	  #print("Y")
 	  #print(delta)
 	}
@@ -125,8 +155,14 @@ for (epoca in 1:ep){
         E3[epoca]=mean(Erro_quad[epoca,]) 
 	E2[epoca]=mean(Erro_abs[epoca,])   # media dos erros absolutos	
 	EE1[epoca]=sum(EE[epoca,])
-	erro_min[epoca]=min(Erro_quad[epoca,])
+	#erro_min[epoca]=min(Erro_quad[epoca,])
     	erro_max[epoca]=max(Erro_quad[epoca,])
+	#for (m in M:1)
+        #   if( m == 1)
+        #      W[[m]] = W[[m]] + (eta)*(delta[[m]] %*% t(X))
+        #   else
+         #     W[[m]] = W[[m]] + (eta)*(delta[[m]] %*% t(U[[m-1]])) #
+
 }
 #print( Erro_quad[5,60])
 squared_R = c(rep(0,ep))
@@ -140,7 +176,10 @@ plot(Erro_quad[,L],type='l')
 plot(E3,ylab="Media do erro quadrado",type='l')
 plot(E2,ylab="Media do erro Absoluto",type='l')
 plot(squared_R,type='l')
-plot((erro_min),type='l')
+plot(unscale(Ys[,41]),type='l')#,xlim=c(0,1000),ylim=c(0,1))
+#axis(1)
+#axis(side=1, at=c(0:23))
+#axis(side=2, at=seq(0, 600, by=100))
 plot((erro_max),type='l')
 
 #X = matrix( c(1, 1) )
@@ -179,12 +218,13 @@ for ( i in 46:60)
 
 unscale(min(Yd[1,])) == min(Yd_original[1,])
 unscale(max(Yd[1,])) ==max(Yd_original[1,]) 
-print(min(erro_min))
-print(unscale(min(erro_min)))
+#print(min(erro_min))
+#print(unscale(min(erro_min)))
+
 print(min(erro_max))
 print(unscale(min(erro_max)))
 unscale(0)
-unscale(0.6)
+unscale(0.4)
 #a= W[[M]] %*% U[[M-1]]
 #print("U")
 #print( U[m] )
